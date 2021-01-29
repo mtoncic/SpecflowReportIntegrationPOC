89ca80e6fc3398f1e9807d1ac081b61b2558cb08
Milan <milan.toncic@zuehlke.com>
2021-01-29 17:01:44 +0100

    Initial commit

---
+++ Calculator/SimpleCalculatorService.cs
@@ -0,0 +1,10 @@
+﻿namespace Calculator
+{
+    public static class SimpleCalculatorService
+    {
+        public static int SumUpTwoNumbers(int firstNumber, int secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+    }
+}
---
+++ SpecflowReportIntegrationPOC/Steps/CalculatorStepDefinition.cs
@@ -0,0 +1,37 @@
+﻿using Calculator;
+using Shouldly;
+using TechTalk.SpecFlow;
+
+namespace SpecflowReportIntegrationPOC.Steps
+{
+    [Binding]
+    [Scope(Feature = "SimpleCalculator")]
+    public sealed class CalculatorStepDefinitions
+    {
+        private int _firstNumber, _secondNumber, _result;
+
+        [Given("the first number is (.*)")]
+        public void GivenTheFirstNumberIs(int number)
+        {
+            _firstNumber = number;
+        }
+
+        [Given("the second number is (.*)")]
+        public void GivenTheSecondNumberIs(int number)
+        {
+            _secondNumber = number;
+        }
+
+        [When("the two numbers are added")]
+        public void WhenTheTwoNumbersAreAdded()
+        {
+            _result = SimpleCalculatorService.SumUpTwoNumbers(_firstNumber, _secondNumber);
+        }
+
+        [Then("the result should be (.*)")]
+        public void ThenTheResultShouldBe(int result)
+        {
+            _result.ShouldBe(result);
+        }
+    }
+}
